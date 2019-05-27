f083acf2cfc9c7c2bab3f91da380553eb362ee25
matthew <matt.duran@rea-group.com>
2019-05-27 12:29:52 +1000

    word-embedding transformation

--- wordEmbedding.py
+++ wordEmbedding.py
@@ -5,35 +5,37 @@ import gensim
 
 
 # Load Google's pre-trained Word2Vec model.
-# model = gensim.models.KeyedVectors.load_word2vec_format('./model/GoogleNews-vectors-negative300.bin', binary=True)  
+model = gensim.models.KeyedVectors.load_word2vec_format('./model/GoogleNews-vectors-negative300.bin', binary=True)  
 
 
 def avg_word_vector(word):
-  # word_vectors = model.word_vec(word)
-  word_vectors = [10,12,134,13]
+  word_vectors = model.word_vec(word)
   return np.mean(word_vectors)
 
 
 def average_sentence_vector(sentence):
   avg_sentence_vector = []
   for word in sentence:
-    # if word in model.vocab:
-    word_vector = avg_word_vector(word)
-    avg_sentence_vector.append(word_vector)
+    if word in model.vocab:
+      word_vector = avg_word_vector(word)
+      avg_sentence_vector.append(word_vector)
   return np.mean(avg_sentence_vector)
 
-def write_row_to_csv(row): 
-  with open('unclassifiedDataW2V.csv','wb') as file:
-    file.write(row)
+def write_to_csv(row): 
+  with open('foo.csv', 'wb') as abc:
+    np.savetxt(abc, row, delimiter=",", fmt='%s')
 
 
 #get data for the model 
 data = pandas.read_csv("unclassifiedData.csv", usecols=['description'])
-
-for row in data.values:
-  sentence = row[0].lower().split()
+updated_data = []
+for row in data.to_numpy():
+  sentence = row[0].lower().replace(",",'').split()
   sentence_vec = average_sentence_vector(sentence)
+  row[0] = row[0].replace(",","")
   row = np.append(row, sentence_vec)
-  write_row_to_csv(row)
+  updated_data.append(row)
+
+write_to_csv(updated_data)
   
 
