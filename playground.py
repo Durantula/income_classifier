35dd2ee8227122233080f6e35864b1742c534260
matthew <matt.duran@rea-group.com>
2019-05-27 11:22:30 +1000

    add word embedding work

--- playground.py
+++ playground.py
@@ -3,12 +3,19 @@ import pandas
 from sklearn import svm
 # from sklearn.linear_model import SGDClassifier
 from gensim.models import Word2Vec
+from sklearn.neural_network import MLPClassifier
+
+# TODO LIST
+# Add neural net model - DONE
+# add date -- didn't improve 
+# add embedded word vectors 
+# add synthetic data 
 
 
 # import classified data
 data = pandas.read_csv("./data/cleanedClassifiedData.csv", sep=",")
 classifiedCategory = pandas.read_csv("./data/classifiedCategory.csv", sep=",")
-unclassifiedData = pandas.read_csv("./data/cleanedUnclassifiedData.csv", sep=",")
+unclassifiedData = pandas.read_csv("./data/unclassifiedCleaned.csv", sep=",")
 # data = pandas.read_csv("classifiedData.csv", header = 0)
 
 
@@ -16,13 +23,15 @@ unclassifiedData = pandas.read_csv("./data/cleanedUnclassifiedData.csv", sep=","
 
 # only get the numerical data from what was read
 data = data._get_numeric_data()
+print data
 
-X = data.values
+
 y = classifiedCategory.values.flatten()
 
 
 # clf = SGDClassifier(loss="hinge", penalty="l2", max_iter=5)
 clf = svm.SVC(gamma='scale')
+# clf = MLPClassifier()
 clf.fit(X,y)
 
 # output = clf.predict(unclassifiedData.values)
--- wordEmbedding.py
+++ wordEmbedding.py
@@ -1,24 +1,39 @@
 import numpy as np
 import pandas
-from gensim.models import Word2Vec
+# from gensim.models import Word2Vec
+import gensim
 
 
-sentences = []
+# Load Google's pre-trained Word2Vec model.
+# model = gensim.models.KeyedVectors.load_word2vec_format('./model/GoogleNews-vectors-negative300.bin', binary=True)  
+
+
+def avg_word_vector(word):
+  # word_vectors = model.word_vec(word)
+  word_vectors = [10,12,134,13]
+  return np.mean(word_vectors)
+
+
+def average_sentence_vector(sentence):
+  avg_sentence_vector = []
+  for word in sentence:
+    # if word in model.vocab:
+    word_vector = avg_word_vector(word)
+    avg_sentence_vector.append(word_vector)
+  return np.mean(avg_sentence_vector)
+
+def write_row_to_csv(row): 
+  with open('unclassifiedDataW2V.csv','wb') as file:
+    file.write(row)
+
 
 #get data for the model 
 data = pandas.read_csv("unclassifiedData.csv", usecols=['description'])
 
 for row in data.values:
   sentence = row[0].lower().split()
-  sentences.append(sentence)
+  sentence_vec = average_sentence_vector(sentence)
+  row = np.append(row, sentence_vec)
+  write_row_to_csv(row)
+  
 
-# train model
-model = Word2Vec(sentences, min_count=200)
-
-
-#store model 
-model.save('mymodel')
-
-
-#apply model to the unsupervised version 
-# model['beem']
